0eba217f61bf6de76aa155bcb3aeee1bc3722cb8
paviii006 <43036871+paviii006@users.noreply.github.com>
2018-09-07 11:17:26 +0530

    Add files via upload

---
+++ arithmetic calculation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a=3;
+    b=4;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("sum"+c);
+    System.out.println("sub"+d);
+    System.out.println("mul"+e);
+    System.out.println("div"+f);
+  }
+}
