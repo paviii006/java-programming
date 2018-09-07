0fab5b941de5e79e5e24014d78086c2e4db0e01b
paviii006 <43036871+paviii006@users.noreply.github.com>
2018-09-07 11:40:04 +0530

    Add files via upload

---
+++ floating point arithmetic operation.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b,c,d,e,f;
+    a=3;
+    b=4.5;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("sum"+c);
+    System.out.println("sub"+d);
+    System.out.println("mul"+e);
+    System.out.println("div"+f);
+  }
+}
