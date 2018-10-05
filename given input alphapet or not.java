756eaf7f67d13bc8b4d75ebf7c1500bc80b5e256
paviii006 <43036871+paviii006@users.noreply.github.com>
2018-10-05 11:33:51 +0530

    Add files via upload

---
+++ given input alphapet or not.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    char c = 's';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
+    
